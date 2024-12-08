9f29d451fe906a629ee4e5c9a42a0c313f02f159
JulianCmeijer <145493082+JulianCmeijer@users.noreply.github.com>
2024-12-08 17:57:52 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,4 @@
-// JavaScript Document
-console.log("hi");
+document.getElementById("hamburger").addEventListener("click", function() {
+    const menuList = document.getElementById("menuList");
+    menuList.classList.toggle("slide-in"); // Toggle the slide-in class
+});
